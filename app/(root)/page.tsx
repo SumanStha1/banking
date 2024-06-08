e2818060e097c7616a60ce9203bd87aea8378483
SumanStha1 <shresthasuman757@gmail.com>
2024-06-08 11:05:30 +0545

    Implemet complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,27 +1,38 @@
-import HeaderBox from '@/components/ui/HeaderBox'
-import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
+import HeaderBox from "@/components/ui/HeaderBox";
+import Rightsidebar from "@/components/ui/Rightsidebar";
+import TotalBalanceBox from "@/components/ui/TotalBalanceBox";
 
 const Home = () => {
-        const loggedIn = {firstName: 'Suman'};
+  const loggedIn = {
+    firstName: "Suman",
+    lastName: "Shrestha",
+    email: "suman@gmail.com",
+  };
   return (
-    <section className= "home">
-        <div className= "home-content">
-            <header className="home-header">
-            <HeaderBox 
-                type="greeting"
-                title="Welcome"
-                user= {loggedIn?.firstName || 'Guest'}
-                subtext="Access and manage your account and transactions efficiently."
-            />
-            <TotalBalanceBox 
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
             accounts={[]}
             totalBanks={1}
             totalCurrentBalance={1250.77}
-            />
-            </header>
-        </div>
+          />
+        </header>
+        RECENT TRANSACTION
+      </div>
+      <Rightsidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 1250.3 }, { currentBalance: 200.3 }]}
+      />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
