d8a97026bec9ee1e228d2e7517469f6bc36b4284
SumanStha1 <shresthasuman757@gmail.com>
2024-06-08 15:30:13 +0545

    Implement sign in and sign up form

---
+++ components/ui/AuthForm.tsx
@@ -0,0 +1,138 @@
+'use client';
+import Image from 'next/image'
+import Link from 'next/link'
+import React, { useState } from 'react'
+
+
+import { date, z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import CustomInput from './CustomInput';
+import { authFormSchema } from '@/lib/utils';
+import { Loader2 } from 'lucide-react';
+
+
+const AuthForm = ({ type }: { type: string }) => {
+    const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const formSchema = authFormSchema(type);
+
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        setIsLoading(true)
+        console.log(values)
+        setIsLoading(false)
+    }
+    return (
+        <section className="auth-form">
+            <header className='flex flex-col gap-5 md: gap-8'>
+                <Link
+                    href="/"
+                    className="cursor-pointer flex items-center gap-1"
+                >
+                    <Image
+                        src="/icons/logo.svg"
+                        width={34}
+                        height={34}
+                        alt="Abm Logo"
+                    />
+                    <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
+                        ABM Banking
+                    </h1>
+                </Link>
+                <div className='flex fle-col gap-1 md:gap-3'>
+                    <h1 className='text-24 lg:text-36 font-semibold text-gray-900'>
+                        {user
+                            ? 'Link Account'
+                            : type === 'sign-in'
+                                ? 'Sign In'
+                                : 'Sign Up'
+                        }
+                        <p className="text-16 font-normal text-gray-600">
+                            {user
+                                ? 'Link your account to get started'
+                                : 'Please enter your details'
+                            }
+                        </p>
+                    </h1>
+                </div>
+            </header>
+            {user ? (
+                <div className="flex flex-col gap-4">
+                    {/* Plaid Lin k */}
+                </div>
+            ) : (
+                <>
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                            {type === 'sign-up' && (
+                                <>
+                                    <div className="flex gap-4">
+                                        <CustomInput control={form.control} name="firstName" label="First Name" placeholder="Enter your first name" type={''} />
+                                        <CustomInput control={form.control} name="lastName" label="Last Name" placeholder="Enter your last name" type={''} />
+                                    </div>
+                                    <CustomInput control={form.control} name="address1" label="Address" placeholder="Enter your specific address" type={''} />
+                                    <div className="flex gap-4">
+                                        <CustomInput control={form.control} name="state" label="State" placeholder="Example: NY" type={''} />
+                                        <CustomInput control={form.control} name="postalcode" label="Postal Code" placeholder="Example: 2154" type={''} />
+                                    </div>
+                                    <div className="flex gap-4">
+                                        <CustomInput control={form.control} name="dateOfBirth" label="Date of Birth" placeholder="YYYY-MM-DD" type={''} />
+                                        <CustomInput control={form.control} name="ssn" label="SSN" placeholder="Example:1254" type={''} />
+                                    </div>
+
+                                </>
+                            )}
+                            <CustomInput control={form.control} name="email" label="Email" placeholder="Enter your email" type={''} />
+                            <CustomInput control={form.control} name="password" label="Password" placeholder="Enter your password" type="password" />
+
+                            <div className="flex flex-col gap-4">
+                                <Button type="submit" disabled={isLoading} className='form-btn'>
+                                    {isLoading ? (
+                                        <>
+                                            <Loader2 size={20} className='animate-spin' /> &nbsp;
+                                            Loading...
+                                        </>
+                                    ) : type === 'sign-in'
+                                        ? 'Sign In' : 'Sign Up'}
+                                </Button>
+                            </div>
+                        </form>
+                    </Form>
+                    <footer className='flex justify-center gap-1'>
+                        <p className="text-14 font-normal text-gray-600">{type === 'sign-in'
+                            ? "Don't have an account?"
+                            : "Already have an account?"
+                        }
+                        </p>
+                        <Link href={type === 'sign-in' ? 'sign-up' : '/sign-in'} className="form-link">{type === 'sign-in' ? 'Sign Up' : 'Sign In'}</Link>
+                    </footer>
+                </>
+            )}
+        </section>
+    )
+}
+
+export default AuthForm
